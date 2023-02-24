7d8ee2a96032bef882ae214e9dbbb5ceba001e89
alejito871026 <dahdhenao@gmail.com>
2023-02-24 18:55:34 -0500

    src/app.js 3

--- src/database/keys.js
+++ src/database/keys.js
@@ -1,8 +1,11 @@
 module.exports = {
     mongodb:{
-        uri :'mongodb://127.0.0.1:27017/taller1',
+        uri :'mongodb+srv://dahdhenao:8R1zKuiPXPoxQb3B@taller1.8xywlmt.mongodb.net/?retryWrites=true&w=majority',
             options:{
             useNewUrlParser:true, useUnifiedTopology: true 
         }   
     }
 } 
+// 'mongodb://127.0.0.1:27017/taller1'
+// mongodb+srv://dahdhenao:<password>@taller1.8xywlmt.mongodb.net/?retryWrites=true&w=majority
+// pass   8R1zKuiPXPoxQb3B mongodb taller1
